82700c16c1c1c68e2004f177a0b188d2718f358b
pomfy113 <pomfy113@gmail.com>
2017-09-06 01:05:53 -0700

    Occurence Check
    Basic check of how many times word occurs; needs clean-up

--- histogram.py
+++ histogram.py
@@ -6,12 +6,16 @@ testfilename = "egg fish egg fish James James James "
 # def histogram(filename):
 # dictionary = open(filename).read().split
 dictionary = testfilename.split()
-testlist = []
 templist = []
-j = 0
+alreadyplaced = []
+j = -1
 for i in range(len(dictionary)):
-    if dictionary[i] in testlist:
-        templist.append((dictionary[i], j+1))
-        print templist
-    testlist.append(dictionary[i])
+    if dictionary[i] in alreadyplaced:
+        templist[alreadyplaced.index(dictionary[i])] = [templist[alreadyplaced.index(dictionary[i])][0], templist[j][1]+1]
+    else:
+        templist.append((dictionary[i], 1))
+        alreadyplaced.append(dictionary[i])
+        j = j+1
+    #print j
     print dictionary[i]
+    print templist
